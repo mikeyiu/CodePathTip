8c07d0572e9d62831bf8dcc6fbbb16578eaf2f66
Mike Yiu <mikesun@mikes-air.home>
2018-12-22 22:25:00 -0500

    Initial Commit

---
+++ CodePathTip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodePathTip
+//
+//  Created by Mike Sun on 12/22/18.
+//  Copyright © 2018 Mike Yiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
